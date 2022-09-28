3c266007781d467bce57b486fc080b255151390d
BuildTools <17001900@students.sd308.org>
2022-09-27 23:41:15 -0500

    Finished Example BAB (untested)

---
+++ src/main/java/frc/robot/Globals.java
@@ -0,0 +1,7 @@
+package team.gif.robot;
+
+public abstract class Globals {
+    // used as a flag to allow classes to indicate they have control of the CIM motor
+    // and the joystick input should be ignored
+    public static boolean g_buttonControl;
+}
